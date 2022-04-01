8c5de4713ac21ed5fd342b5d98cce4efcb1ae43a
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-04-01 23:05:16 +0200

    Add funtion to simulate computer play. Implement check

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function computerPlay() {
+  const computerOptions = ['Rock', 'Paper', 'Scissors'];
+  let optionRandomizer = Math.floor(Math.random() * computerOptions.length);
+  let computerChoice = computerOptions[optionRandomizer];
+  console.log(optionRandomizer, computerChoice);
+  return computerChoice;
+}
+
+let rock = 0;
+let paper = 0;
+let scissors = 0;
+
+for (let i = 0; i < 10000; i++) {
+  let toCount = computerPlay();
+  switch (toCount) {
+    case 'Rock':
+      rock++;
+      break
+    case 'Paper':
+      paper++;
+      break
+    case 'Scissors':
+      scissors++;
+      break
+  }
+}
+
+console.log(rock, paper, scissors);
