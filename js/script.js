18be53efd5b984e7e760ee316e14bad1bf4f3ee0
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-04-15 02:25:57 +0200

    Add winner div, add button disable on game end, add reset button and functionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 // game(); autostarts game
 
-const buttons = document.querySelectorAll('button'); // select buttons from document
+const buttons = document.querySelectorAll('.choices'); // select buttons from document
 let pointsPlayer = 0;
 let pointsComputer = 0;
 const rounds = 1;
 const gameResult = document.querySelector('.game-result');
 const score = document.querySelector('.score');
+const winner = document.querySelector('.chicken-dinner');
+const reset = document.querySelector('.game-reset');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', function(e) {
+  button.addEventListener('click', function (e) {
     playRound(e.target.innerHTML, computerPlay()); //run a round when a button is clicked. Listener for each button
+  });
+});
+
+reset.addEventListener('click', () => {
+  gameResult.textContent = "";
+  pointsPlayer = 0;
+  pointsComputer = 0;
+  winner.textContent = "";
+  score.textContent = 'Current score: 0 - 0';
+  buttons.forEach((button) => {
+    button.disabled = false;
   });
 });
 
@@ -26,17 +39,28 @@ function playRound(playerSelection, computerSelection) {
   const gameContent = `${playerSelection}${computerSelection}`;
   const optionSet = new Set(["RockScissors", "PaperRock", "ScissorsPaper"]);
   if (playerSelection === computerSelection) {
-    gameResult.textContent = `Tie! You both chose ${playerSelection}...`;
+    gameResult.textContent = `Computer chooses ${computerSelection}. It's a tie! You both chose ${playerSelection}...`;
     pointsPlayer += 1;
     pointsComputer += 1;
   } else if (optionSet.has(gameContent)) {
-    gameResult.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+    gameResult.textContent = `Computer chooses ${computerSelection}. You win! ${playerSelection} beats ${computerSelection}!`;
     pointsPlayer += 2;
   } else {
-    gameResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    gameResult.textContent = `Computer chooses ${computerSelection}. You lose! ${computerSelection} beats ${playerSelection}!`;
     pointsComputer += 2;
   }
   score.textContent = `Current score: ${pointsPlayer} (You) - ${pointsComputer} (Computer)`;
+
+  if (pointsComputer >= 10 || pointsPlayer >= 10) {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+    if (pointsComputer < pointsPlayer) {
+      winner.textContent = 'You win!';
+    } else {
+      winner.textContent = 'Computer beats human :-(!';
+    }
+  }
 }
 
 function game() {
