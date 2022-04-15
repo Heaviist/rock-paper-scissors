cd3d58f2c594b53f64c6293e84bb0a72c435e81a
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-04-15 02:29:11 +0200

    Remove original game function and other old code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// game(); autostarts game
-
 const buttons = document.querySelectorAll('.choices'); // select buttons from document
 let pointsPlayer = 0;
 let pointsComputer = 0;
@@ -32,7 +30,6 @@ function computerPlay() {
   let computerResult = computerOptions[optionRandomizer];
   console.log(optionRandomizer, computerResult);
   return computerResult;
-  // return computerResult; //Uncomment to run check section
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -62,74 +59,3 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 }
-
-function game() {
-  let points = 0;
-
-  // const rounds = +prompt('How many rounds do you want to play? (Enter an integer, 5 is recommended)', '5'); //uncomment to play multiple rounds
-  const rounds = 1; // comment out for multiple rounds
-
-  // uncomment below code to return to playing multiple rounds and have more choices. Current state plays 1 round
-  //  if (Number.isInteger(rounds)) {
-  //    if (confirm(`This game of Rock Paper Scissors consists of ${rounds} rounds. Choose "OK" if you want to use the same choice for all ${rounds} rounds. Choose "No" if you want to make a new choice for every round.`)) {
-  //      for (let i = 0; i < rounds; i++) {
-  //        let playerResult = playRound(playerInput(), computerPlay()); //playerChoice will be the same for the whole round, computerplay will be generated every round
-  //        points = points + playerResult;
-  //      }
-  //    } else {
-  //      let choice = playerInput();
-  //      for (let i = 0; i < rounds; i++) {
-  //        let playerResult = playRound(choice, computerPlay()); //playerChoice will be the same for the whole round, computerplay will be generated every round
-  let playerResult = playRound(playerInput(), computerPlay()); // remove when returning to multiround games
-  points = points + playerResult;
-  //      }
-  //    }
-  alert(`You scored ${points / 2} points, computer scored ${(rounds * 2 - points) / 2}!`)
-  //  } else {
-  //    alert(`That's not an integer. Please enter an integer (a positive whole number)`);
-  //    game();
-  //  }
-}
-
-/* // obsolete when using UI
-function playerInput() {
-  let choice = ''
-  choice = prompt('Type "Rock", "Paper" or "Scissors" to start a game:').toLowerCase();
-  if (choice == "rock" || choice == "paper" || choice == "scissors") {
-    console.log(choice);
-    return choice;
-  } else {
-    alert(`${choice} is invalid input`);
-    return playerInput();
-  }
-}
-*/
-
-// const playerChoice = prompt('Choose "Rock", "Paper" or "Scissors"').toLowerCase();
-// const computerChoice = computerPlay(); generated every time when playing multiple rounds
-// playRound(playerChoice, computerChoice); will be invoked from game() for multiple rounds
-
-/*
-
-//Check and print i repetitions of computerPlay
-
-let rock = 0;
-let paper = 0;
-let scissors = 0;
-
-for (let i = 0; i < 10000; i++) {
-  let toCount = computerPlay();
-  switch (toCount) {
-    case 'rock':
-      rock++;
-      break
-    case 'paper':
-      paper++;
-      break
-    case 'scissors':
-      scissors++;
-      break
-  }
-}
-
-console.log(rock, paper, scissors);*/
